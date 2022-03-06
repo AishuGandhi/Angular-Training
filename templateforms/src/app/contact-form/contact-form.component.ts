4192368e21e132d16135be661cdbf1e79de65753
Aishwarya <aishwarya.gandhi@finastra.com>
2022-03-06 21:23:41 +0530

    Template Forms Done

--- templateforms/src/app/contact-form/contact-form.component.ts
+++ templateforms/src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-form',
+  selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  contactMethods = [
+    {id:1,name:"Email"},
+    {id:2,name:"Phone"}
+  ];
+  log(firstName : any){
+        console.log(firstName);
+  }
+  submit(f:any){
+    console.log(f);
+    console.log(f.value);
   }
 
 }
